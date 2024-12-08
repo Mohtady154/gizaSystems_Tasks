100e84338ba993be75b607296a8925ed24e133fb
Mohtady154 <mohtadysameh154@gmail.com>
2024-12-08 18:10:58 +0200

    changing in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import UserForm from "./components/UserForm";
 import UserTable from "./components/UserTable";
 
